6a989ba04631dba7898faae490a6def321ff0647
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-07-27 18:47:31 +0500

    bla bla bla

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Header, Categories } from './components'
 
 function App() {
 
+   
+
   return (
     <div className="wrapper">
 
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 
+
+// class Categories extends React.Component {
+
+//   state = {
+//     activeItem: 3,
+//   };
+
+//   onSelectItem = (index) => {
+//     this.setState({
+//       activeItem: index,
+//     });
+//    };
+
+//   render() {
+//     const { items} = this.props;
+//     return (
+//       <div className="categories">
+//         <ul>
+//           <li>Все</li>
+//           {
+//             items.map((name, index) => <li 
+//               className={this.state.activeItem === index ? "active" : ''} 
+//               onClick={() => this.onSelectItem(index)} 
+//               key={`${name}_${index}`}
+//               >
+//                 {name}
+//               </li>)
+//           }
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
 function Categories({ items, onClickItem }) {
+
+  const [activeItem, setActiveItem] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
+        <li>Все</li>
         {
-          items.map((name, index) => <li onClick={() => onClickItem(name)} key={`${name}_${index}`}>{name}</li>)
+          items.map((name, index) =>
+            <li className={activeItem === index ? "active" : ''} onClick={() => onSelectItem(index)} key={`${name}_${index}`}>
+              {name}
+            </li>)
         }
       </ul>
     </div>
